4a69fd19c5996cef13eb308f06941bfc65b46919
Zhari7 <meneseszharick29@gmail.com>
2022-09-24 16:40:33 -0500

    ejer_2

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ addEventListener("DOMContentLoaded", (e) => {
     let form = document.getElementById("form");
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-    let num = document.getElementById("numero").value;
-    if (num >= 0 && num <= 100) {
-        document.getElementById("resul").innerHTML="El número es positivo y menor que 100";
+
+    let num1 = document.getElementById("numero1").value;
+    let num2 = document.getElementById("numero2").value;
+    let num3 = document.getElementById("numero3").value;
+
+    if (num1 > num2 && num1 > num3 ) {
+        document.getElementById("resul").innerHTML="El número mayor es: " + num1;
     }
+
+    else if ( num1 < num2 && num2 > num3 ) {
+        document.getElementById("resul").innerHTML="El número mayor es: " + num2;
+    }
+
     else {
-        document.getElementById("resul").innerHTML="El número es negativo o mayor que 100";
+        document.getElementById("resul").innerHTML="El número mayor es: " + num3;
     }
     })
 })
